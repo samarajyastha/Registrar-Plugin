e3c6b5164ea81b5d8e8e942d5a2c1827c462af96
cizzan <samarajyastha@gmail.com>
2021-08-27 13:17:54 +0545

    User rating with added design

--- includes/class-registrar-users.php
+++ includes/class-registrar-users.php
@@ -10,38 +10,54 @@
 
     // Methods
     function __construct() {
-      $users = get_users();
-      foreach($users as $user) {
-         // print_r($user);
-      }
        $this->users_template();
     }
 
+    // Show all users in a card.
     function users_template() {
-       $users = get_users();
        ?>
        <div class="registrar-container">
          <div class="row">
-            <?php foreach( $users as $user ) { ?>
-               <div class="col-md-4 col-sm-6 col-12">
-                  <div class="card">
-                     <div class="card-body">
-                        <h3 class="card-title"><?php echo $user->display_name; ?></h3>
-                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $user->user_email; ?></h6>
-                        <p class="card-text"><?php echo get_user_meta( $user->ID, 'description' )[0] ? get_user_meta( $user->ID, 'description' )[0] : "No reviews."; ?></p>
-                        <div class="reviews">
-                           <i class="fa fa-star"></i>
-                           <i class="fa fa-star"></i>
-                           <i class="fa fa-star"></i>
-                           <i class="fa fa-star"></i>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            <?php } ?>
+            <?php $this->users_grid(); ?>
          </div>
-      </div>
+       </div>
       <?php
+    }
+
+    // Single user card with name, email, reviews and rating.
+    function users_grid(){
+       
+      // Get all users from wp_users() table.
+      $users = get_users();
+
+      foreach( $users as $user ) { ?>
+
+         <div class="col-md-4 col-sm-6 col-12">
+            <div class="card">
+               <div class="card-body">
+
+                  <h3 class="card-title"><?php echo $user->display_name; ?></h3>
+                  <a href="mailto:<?php echo $user->user_email; ?>"><h6 class="card-subtitle mb-2"><?php echo $user->user_email; ?></h6></a>
+                  <p class="card-text"><?php echo get_user_meta( $user->ID, 'description' )[0] ? get_user_meta( $user->ID, 'description' )[0] : "No reviews."; ?></p>
+                  
+                  <div class="reviews">
+                     <?php 
+                        // Users rating
+                        $rating = get_user_meta( $user->ID, 'rating' );
+                        if( ! empty( $rating ) ) {
+                           while( $rating[0] > 0 ) {
+                              echo ' <i class="fa fa-star"></i> ';
+                              $rating[0]--;
+                           }
+                        }
+                     ?>
+                  </div>
+
+               </div>
+            </div>
+         </div>
+
+      <?php }
     }
 
  }
